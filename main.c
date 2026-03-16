e77313cfce183941e9d62ac9d6108c91a8095689
zeadab <ist.abadi.ali@gmail.com>
2026-03-16 21:42:19 +0100

    Initial commit

---
+++ main.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+int getComputerChoice();
+int getUserChoice();
+void checkWinner(int userChoice,int computerChoice);
+
+int main(void)
+{
+    int computerChoice = getComputerChoice();
+    int userChoice = getUserChoice();
+
+
+}
